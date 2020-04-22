4ce0a5d6a9a61c8df5ba768441e7cd78df2bfe0e
josue <josue@localhost.localdomain>
2020-04-22 01:17:35 +0000

    Tablero de impresoras muestra el objeto json pasado por MQTT

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,76 +1,12 @@
 import React,{Component} from 'react';
 import logo from './logo.svg';
 import './App.css';
-
-
 import Paho from 'paho-mqtt';
-// import { Client, Message } from 'paho-mqtt';
-
-
-// //Set up an in-memory alternative to global localStorage
-
-// const myStorage = {
-//   setItem: (key, item) => {
-//     myStorage[key] = item;
-//   },
-//   getItem: (key) => myStorage[key],
-//   removeItem: (key) => {
-//     delete myStorage[key];
-//   },
-// };
-
-// // Create a client instance
-// const client = new Client({ uri: 'ws://192.168.1.18:1883', clientId: 'clientId', storage: myStorage });
- 
-// // set event handlers
-// client.on('connectionLost', (responseObject) => {
-//   if (responseObject.errorCode !== 0) {
-//     console.log(responseObject.errorMessage);
-//   }
-// });
-// client.on('messageReceived', (message) => {
-//   console.log(message.payloadString);
-// });
- 
-// // connect the client
-// client.connect()
-//   .then(() => {
-//     // Once a connection has been made, make a subscription and send a message.
-//     console.log('onConnect');
-//     return client.subscribe('World');
-//   })
-//   .then(() => {
-//     const message = new Message('Hello');
-//     message.destinationName = 'World';
-//     client.send(message);
-//   })
-//   .catch((responseObject) => {
-//     if (responseObject.errorCode !== 0) {
-//       console.log('onConnectionLost:' + responseObject.errorMessage);
-//     }
-//   })
-// ;
-
-
-
-
-
-
-//import init from 'react_native_mqtt';
-//import { AsyncStorage } from 'react-native';
-//ar hostname = "m21.cloudmqtt.com";
-//var port = 37719;
-//var hostname="ws:192.168.1.18"
-//var clientId = "myClientId";
-//var clientId = new Date().getUTCMilliseconds();;
-//var username = "webclient";
-//var password = "Super$icher123";
-
-var subscription = "prueba";
+import PrinterInformation from './itemDashboard.js';
 
 function onConnect() {
-  console.log("onConnect josue");
-  mqttCli.subscribe(subscription);
+ console.log("onConnect josue");
+ mqttCli.subscribe(subscription);
 }
 
 function onConnectionLost(responseObject) {
@@ -79,38 +15,43 @@ function onConnectionLost(responseObject) {
   }
 }
 
-function onMessageArrived(message) {
-  console.log("onMessageArrived:"+message.payloadString);
-
-}
-
-function Connected() {
-  console.log("Connected, estamos conectectados");
-  mqttCli.subscribe(subscription);
-}
-
-//var mqttCli = new Paho.Client(hostname,Number(port), clientId);
-var mqttCli = new Paho.Client("ws://192.168.1.18:9001/mqtt", "myCLientId" + new Date().getTime())
-//var mqttCli = new Paho.MQTT.Client(hostname, clientId + new Date().getTime())
-//var mqttCli = new Paho.mqttCli(hostname, clientId + new Date().getTime())
-
+var mqttCli=new Paho.Client("ws://192.168.1.18:9001/mqtt", "myCLientId" + new Date().getTime())
+var subscription="prueba";
 mqttCli.connect({ onSuccess: onConnect})
-//mqttCli.connect({ onSuccess: onConnect })
-// establecer manejadores de devolución de llamada
-mqttCli.onMessageArrived = onMessageArrived;
 mqttCli.onConnectionLost = onConnectionLost;
 
-class MensajeMQTT extends Component{
-  //state= {hora: new Date().toLocaleTimeString()}
-  state= {texto: "hola"}
+class ManagerMQTT extends Component{
+
+  state={impresora:{}}
+  onMessageArrived = message => {
+    this.setState({impresora: JSON.parse(message.payloadString)});
+}
+
+  componentDidMount() {
+     this.props.mqttCli.onMessageArrived = this.onMessageArrived;
+  }
   render(){
     return(
-      <p> {this.state.texto} </p>
+      <div>
+        <table>
+          <thead>
+            <tr>
+              <th>Name</th>
+              <th>Temperature</th>
+            </tr>
+          </thead>
+          {<tbody><PrinterInformation printer={this.state.impresora}/></tbody>}
+        </table>
+
+      </div>
     )
   }
 }
 
+
+
 class App extends Component {
+
   render(){
     return (
       <div className="App">
@@ -132,9 +73,10 @@ class App extends Component {
  
       <h1>Tablero sobre estado de las impresoras </h1>
       <div>
-          <MensajeMQTT />
-
-
+          <ManagerMQTT 
+          mqttCli={mqttCli}
+          suscription={subscription}
+          />
       </div>
 
       </div>    
