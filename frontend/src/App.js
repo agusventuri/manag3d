c380614a9e75acd7e4e2e371614e32682748918c
josue <josue@localhost.localdomain>
2020-04-23 15:50:45 +0000

    dashboard funcionando

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,8 +1,11 @@
-import React,{Component} from 'react';
+import React,{Component, useState} from 'react';
 import logo from './logo.svg';
 import './App.css';
 import Paho from 'paho-mqtt';
+import './itemDashboard.css';
 import PrinterInformation from './itemDashboard.js';
+
+
 
 function onConnect() {
  console.log("onConnect josue");
@@ -22,27 +25,99 @@ mqttCli.onConnectionLost = onConnectionLost;
 
 class ManagerMQTT extends Component{
 
-  state={impresora:{}}
+  state={impresora:[{
+    "printer_id": 0,
+    "printer_name": "",
+    "printer_state": "",
+    "jobs": [
+      {
+        "start_time":0,
+        "finish_time": 0,
+        "completion": 0,
+        "print_time": 0,
+        "print_time_left":0,
+        "customer":"",
+        "file": {
+          "id":0,
+          "name":"",
+          "estimated_time":0
+        }
+      }]
+    }]
+  }
+
   onMessageArrived = message => {
-    this.setState({impresora: JSON.parse(message.payloadString)});
+    //esto pisa el state
+    //this.setState({impresora: JSON.parse(message.payloadString)});
+
+    //actualizamos estados de impresoras que ya están en el dashboard
+    this.setState({impresora:  Object.keys(this.state.impresora).map( imp => {
+            // console.log("imp: "+this.state.impresora[imp].printer_id)
+            // console.log("esto comp el if: "+this.state.impresora[imp].printer_id+" "+JSON.parse(message.payloadString)[0].printer_id)
+            if (this.state.impresora[imp].printer_id===0){
+              return (JSON.parse(message.payloadString)[0])
+            }else{
+              if(this.state.impresora[imp].printer_id===JSON.parse(message.payloadString)[0].printer_id){
+                return (JSON.parse(message.payloadString)[0])
+            }else{
+                
+                //return JSON.parse( (this.state.impresora).push( JSON.parse(message.payloadString)[0] ) ) 
+                return this.state.impresora[imp]
+              } 
+            }
+            
+          }
+      )
+    }
+    )
+    var estaEnDash = false;
+    
+
+    Object.keys(this.state.impresora).forEach(key => {
+      //console.log("esto compara "+this.state.impresora[key].printer_id+"  "+JSON.parse(message.payloadString)[0].printer_id)
+      if (this.state.impresora[key].printer_id===JSON.parse(message.payloadString)[0].printer_id) {//esta comparacion esta ok
+          estaEnDash = true;
+        }
+      });
+    if(estaEnDash===false){
+        //si es una imp que no esta en el dashboard, la agrego al state
+        var estado= this.state.impresora
+        console.log("NO EST EN DASHBOARD")
+        console.log("ESTO PUSHEO  "+  JSON.parse(message.payloadString)[0] )
+        //this.setState({impresora: (this.state.impresora).unshift( JSON.stringify( JSON.parse(message.payloadString)[0] ) ) })
+        estado.push(JSON.parse(message.payloadString)[0])
+        console.log("var estado: "+estado)
+        this.setState({impresora:estado})
+     }
+
+    //this.setState(newState);
+
 }
 
   componentDidMount() {
      this.props.mqttCli.onMessageArrived = this.onMessageArrived;
+     console.log("se ejecuto didmount")
   }
   render(){
+      console.log("state  :"+this.state.impresora)
+      // console.log("primer impresora "+this.state.impresora[0])
+      // console.log("primer id "+this.state.impresora[0].printer_id)
+      // console.log("cant items"+this.state.impresora.length)
+      if(this.state.impresora[0].printer_id===0){
+        return null
+      }
     return(
       <div>
-        <table>
+      <table>
           <thead>
-            <tr>
-              <th>Name</th>
-              <th>Temperature</th>
+            <tr >
+              <th className="titLeft">Name</th>
+              <th className="titCenter">State</th>
+              <th className="titCenter">Jobs</th>
             </tr>
           </thead>
           {<tbody><PrinterInformation printer={this.state.impresora}/></tbody>}
         </table>
-
       </div>
     )
   }
@@ -71,7 +146,7 @@ class App extends Component {
 
           </header>
  
-      <h1>Tablero sobre estado de las impresoras </h1>
+      <h1 className="titDashboard">Tablero sobre estado de las impresoras </h1>
       <div>
           <ManagerMQTT 
           mqttCli={mqttCli}
--- frontend/src/itemDashboard.js
+++ frontend/src/itemDashboard.js
@@ -1,18 +1,38 @@
 import React,{Component} from 'react';
-import logo from './logo.svg';
 import './App.css';
+import './itemDashboard.css'
 
 class PrinterInformation extends Component{
 	_renderPrinters(){
-		//mostramos nombre, temperatura
 		const {printer}=this.props;
 		console.log(printer);
+		console.log("este es el estado"+printer.printer_state);
 
-		return Object.keys(printer).map(currency =>(
-
-					<tr key={currency}>
-						<td > {printer[currency].name} </td>
-						<td >{printer[currency].temperature} </td>
+		return Object.keys(printer).map(currency =>( //cada CURRENCY es el indice de la impresora en el JSON(0,1,2..)
+					<tr key={currency}> 
+						<td >{printer[currency].printer_name} </td>
+						<td  >{printer[currency].printer_state} </td>
+						{ Object.keys(printer[currency].jobs).map(jobcurrency =>
+							<td key={jobcurrency}>
+							 	<div className="job">
+							 		<p>Start time: {printer[currency].jobs[jobcurrency].start_time}</p>
+							 		<p>Finish time: {printer[currency].jobs[jobcurrency].finish_time}</p>
+							 		<p>Completion: {printer[currency].jobs[jobcurrency].completion} %</p>
+							 		<p>Print time: {printer[currency].jobs[jobcurrency].print_time}</p>
+							 		<p>Print time left: {printer[currency].jobs[jobcurrency].print_time_left}</p>
+							 		<table className="file">
+							 			<tbody>
+							 				<tr>
+							 					 <td>File name: {printer[currency].jobs[jobcurrency].file.name}</td>
+							 				</tr>
+							 				<tr>
+							 					 <td>Customer: {printer[currency].jobs[jobcurrency].customer}</td>
+							 				</tr>
+							 			</tbody>
+							 		</table>
+							 	</div>
+							 </td>
+							 )  }
 					</tr>
 					)
 					)
@@ -23,3 +43,15 @@ class PrinterInformation extends Component{
 	}
 }
 export default PrinterInformation
+
+// {
+//           this.state.ideas.map(({title, premises, conclusion}, i) => (
+//             <div key={i} className="card">
+//               <div className="card-body">
+//                 <h1>{title}</h1>
+//                 {premises.map((premise, j) => <p key={j}>{premise}</p>)}
+//                 <p>{conclusion}</p>
+//               </div>
+//             </div>
+//           ))
+//         }
