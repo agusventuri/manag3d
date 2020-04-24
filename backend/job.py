e967b84d9a1f1ab213df96fb3e9673c6704f435d
agusventuri <agus97v@gmail.com>
2020-04-24 15:58:59 -0300

    Arregladas algunas cosas de la fecha y hora

--- backend/job.py
+++ backend/job.py
@@ -6,22 +6,22 @@ class Job:
     def __init__(self, row, print_time, print_time_left, completion):
         self.id = row[0]
         self.customer = row[5]
-        self.completion = completion if completion is not None else "0"
+        self.completion = completion if completion is not None else 0
         self.print_time = print_time
         self.print_time_left = print_time_left
-        self.start_time = None
-        self.finish_time = None
+        self.start_time = 0
+        self.finish_time = 0
         self.file = File(self.id, row[1], row[9], row[10], row[11])
 
     def set_start_time(self, start_time):
-        if self.start_time is None:
+        if self.start_time == 0:
             self.start_time = start_time
 
     def set_finish_time(self, finish_time):
         self.finish_time = finish_time
 
     def update(self, print_time, print_time_left, completion):
-        self.completion = completion if completion is not None else "0"
+        self.completion = completion if completion is not None else 0
         self.print_time = print_time
         self.print_time_left = print_time_left
 
@@ -29,6 +29,7 @@ class Job:
         text = "\nJob id " + str(self.id)
         text += "\n\tProgress: \t" + str(self.completion) + "%"
         text += "\n\tPrint time:\t" + str(self.print_time) + "s"
+        text += "\n\tStart time:\t" + str(self.start_time) + "s"
         text += "\n\tTime left: \t" + str(self.print_time_left) + "s"
         text += "\n\tCustomer: \t" + str(self.customer)
         text += str(self.file)
--- frontend/src/itemDashboard.js
+++ frontend/src/itemDashboard.js
@@ -8,21 +8,25 @@ class PrinterInformation extends Component{
 		const {printer}=this.props;
 		console.log(printer);
 		console.log("este es el estado"+printer.printer_state);
+		console.log("este es el moment", new Date(1587752299*1000))
 
 		return Object.keys(printer).map(currency =>( //cada CURRENCY es el indice de la impresora en el JSON(0,1,2..)
 					<tr key={currency}> 
 						<td >{printer[currency].printer_name} </td>
 						<td  >{printer[currency].printer_state} </td>
-						{ Object.keys(printer[currency].jobs).map(jobcurrency =>
+						{Object.keys(printer[currency].jobs).map(jobcurrency =>
 							<td key={jobcurrency}>
 							 	<div className="job">
-							 		<p>Start time: { moment( (new Date(printer[currency].jobs[jobcurrency].start_time).toString()) ).format("DD-MM-YYYY HH:MM:SS")}</p>
-							 		<p>Finish time: {moment( (new Date(printer[currency].jobs[jobcurrency].finish_time).toString()) ).format("DD-MM-YYYY HH:MM:SS")  }</p>
+									<p>Start time: { moment((new Date((printer[currency].jobs[jobcurrency].start_time)*1000).toString())).format("DD-MM-YYYY HH:MM:SS")}</p>
+									<p>Finish time: { moment((new Date((printer[currency].jobs[jobcurrency].finish_time)*1000).toString())).format("DD-MM-YYYY HH:MM:SS")}</p>
 							 		<p>Completion: {printer[currency].jobs[jobcurrency].completion.toFixed(2)}%</p>
-							 		<p>Print time: { (moment( (new Date(printer[currency].jobs[jobcurrency].print_time).toString()) ).format("hh:mm:ss") )}</p>
-							 		{printer[currency].jobs[jobcurrency].completion >59 
-							 			? <p>Print time left: { (moment( (new Date(printer[currency].jobs[jobcurrency].print_time_left).toString()) ).format("hh:mm:ss") ) }</p>
-							 			:<p>Print time left: {moment( (new Date( printer[currency].jobs[jobcurrency].file.estimated_time -printer[currency].jobs[jobcurrency].print_time ).toString()) ).format("hh:mm:ss") }</p>
+									{/*<p>Print time: { (moment( (new Date((printer[currency].jobs[jobcurrency].print_time)*1000).toString())).format("hh:mm:ss") )}</p>*/}
+									<p>Print time: { Math.floor(printer[currency].jobs[jobcurrency].print_time / 60 / 60) + "hs "
+																		+ (Math.floor(printer[currency].jobs[jobcurrency].print_time / 60) - Math.floor(printer[currency].jobs[jobcurrency].print_time / 60 / 60) * 60) + "m "
+																		+ (printer[currency].jobs[jobcurrency].print_time % 60) + "s"}</p>
+									{printer[currency].jobs[jobcurrency].completion >59
+										? <p>Print time left: { printer[currency].jobs[jobcurrency].print_time_left + "s" }</p>
+										: <p>Print time left: { printer[currency].jobs[jobcurrency].estimated_time - printer[currency].jobs[jobcurrency].print_time + "s" }</p>
 							 		}
 							 		<table className="file">
 							 			<tbody>
